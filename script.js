30497723011ee9e69277481713a2fa7edbab6534
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-02 02:11:24 -0300

    added logic to the js file

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-let message = 'test';
-console.log(message);
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
